bfd65842c1725a9ea2713269b9c7def233eb148c
Gabor Szanto <hello@szantogabor.com>
2011-06-17 01:57:30 +0200

    Refactor overridden theme_form_element add some extra class.

--- sites/all/themes/kogart_admin_theme/template.php
+++ sites/all/themes/kogart_admin_theme/template.php
@@ -3,38 +3,41 @@
 function kogart_admin_theme_form_element($element, $value) {
   // This is also used in the installer, pre-database setup.
 
-  if ($element['#type'] == 'textarea') {
+  $t = get_t();
 
-    $t = get_t();
+  $output = '<div class="form-item form-' . $element['#type'] . '-container"';
+  if (!empty($element['#id'])) {
+    $output .= ' id="' . $element['#id'] . '-wrapper"';
+  }
+  $output .= ">\n";
+  $required = !empty($element['#required']) ? '<span class="form-required" title="' . $t('This field is required.') . '">*</span>' : '';
 
-    $output = '<div class="form-item"';
+  if (!empty($element['#title'])) {
+    $title = $element['#title'];
     if (!empty($element['#id'])) {
-      $output .= ' id="' . $element['#id'] . '-wrapper"';
+      $output .= ' <label for="' . $element['#id'] . '">' . $t('!title: !required', array('!title' => filter_xss_admin($title), '!required' => $required)) . "</label>\n";
     }
-    $output .= ">\n";
-    $required = !empty($element['#required']) ? '<span class="form-required" title="' . $t('This field is required.') . '">*</span>' : '';
-
-    if (!empty($element['#title'])) {
-      $title = $element['#title'];
-      if (!empty($element['#id'])) {
-        $output .= ' <label for="' . $element['#id'] . '">' . $t('!title: !required', array('!title' => filter_xss_admin($title), '!required' => $required)) . "</label>\n";
-      }
-      else {
-        $output .= ' <label>' . $t('!title: !required', array('!title' => filter_xss_admin($title), '!required' => $required)) . "</label>\n";
-      }
+    else {
+      $output .= ' <label>' . $t('!title: !required', array('!title' => filter_xss_admin($title), '!required' => $required)) . "</label>\n";
     }
-
-    if (!empty($element['#description'])) {
-      $output .= ' <div class="description">' . $element['#description'] . "</div>\n";
-    }
-
-    $output .= " $value\n";
-
-    $output .= "</div>\n";
-
-    return $output;
   }
 
-  return theme_form_element($element, $value);
+  if ($element['#type'] == 'textarea') {
+    if (!empty($element['#description'])) {
+      $output .= ' <div class="description">' . $element['#description'] . "</div>\n";
+      //prevent to print out description twice
+      unset($element['#description']);
+    }
+  }
+
+  $output .= " $value\n";
+
+  if (!empty($element['#description'])) {
+    $output .= ' <div class="description">' . $element['#description'] . "</div>\n";
+  }
+
+  $output .= "</div>\n";
+
+  return $output;
 }
 ?>
