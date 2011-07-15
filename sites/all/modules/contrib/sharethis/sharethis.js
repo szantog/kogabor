7037114cbb61e85fe4d49a3921d57d95f5ea30ac
Gabor Szanto <hello@szantogabor.com>
2011-07-15 15:31:48 +0200

    Add module service_links && update sharethis to 6.x-2.x-dev

--- sites/all/modules/contrib/sharethis/sharethis.js
+++ sites/all/modules/contrib/sharethis/sharethis.js
@@ -1,12 +1,27 @@
-// $Id: sharethis.js,v 1.1.2.4 2009/06/05 19:58:57 robloach Exp $
 
 /**
- * Drupal ShareThis behaviors.
+ * The Drupal behaviors for the ShareThis web service.
  */
-Drupal.behaviors.shareThis = function(context) {
-  // Retrieve the ShareThis URL code, if desired.
-  var code = Drupal.settings.sharethisUrl || null;
-
-  // Process each of the ShareThis links.
-  $('.sharethis-link:not(.sharethis-link-processed)', context).addClass('sharethis-link-processed').sharethis(code);
+Drupal.behaviors.sharethis = function(context) {
+  // Process each element.
+  if (Drupal.settings.sharethis) {
+    jQuery.each(Drupal.settings.sharethis, function(id, options) {
+      var element = document.getElementById(id);
+      if (!element) {
+        return;
+      }
+      var shared_object = SHARETHIS.addEntry({
+        title: options['title'],
+        url: options['url']
+      });
+      if (options['chicklet'] || false) {
+        shared_object.attachChicklet(options['chicklet'], element);
+      }
+      else {
+        shared_object.attachButton(element, {button: false});
+      }
+      $('#' + id).click(function() { return false; });
+      delete(Drupal.settings.sharethis[id]);
+    });
+  }
 };
